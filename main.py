1c93a848d7bcdd216aceceb6084a4aeb5f43efd1
bgujjala <bgujjala@deloitte.com>
2021-09-16 14:56:10 +0530

    map the dictionary

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+keys = ['ten', 'twenty', 'thirty']
+values = [10, 20, 30]
+sampledict = dict(zip(keys, values))
+print(sampledict)
